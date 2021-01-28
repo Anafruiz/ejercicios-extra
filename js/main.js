4a6d243465603752d750f791ad61ab1ae1fb2842
Anadbh <anafruiz20@gmail.com>
2021-01-28 16:53:41 +0100

    Add exercise 1 in fase/1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const potatoElement=document.querySelector(".js-potato")
+
+function writePotato (){
+const write= "Patata";
+potatoElement.innerHTML= write;
+return write;
+
+  
+}
+
+potatoElement.innerHTML=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
+potatoElement.innerHTML+=writePotato()
