61fb2d75ed5f56c1648bfcb749b626d6f5c8bd8a
Anadbh <anafruiz20@gmail.com>
2021-01-31 00:00:34 +0100

    Add exercise fase 3

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@
 const foodElement=document.querySelector(".js-food");
 
 function ten(nombre) {
+    for (let i=0;i<10;i++){
+
     foodElement.innerHTML+=nombre;
-}
+
+    }
+} 
 
 ten('Patata');
 ten('Aguacate');
